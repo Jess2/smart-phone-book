5017c4d983db4fb1c72e76353fbfb720fa0029ea
jess2 <jess2.jsy@gmail.com>
2018-12-15 16:35:20 +0900

    태그 삭제 구현

--- src/components/tag/Tag.vue
+++ src/components/tag/Tag.vue
@@ -7,7 +7,7 @@
     <div class="tagBody">
       <ul>
         <li v-for="tag in tagData">
-          <i class="fa fa-minus-circle"></i>{{ tag.name }}
+          <i class="fa fa-minus-circle" @click="alertDeleteTag('tagDelete', tag.id)"></i>{{ tag.name }}
         </li>
         <li>
           <i class="fa fa-plus-circle" @click="addTag"></i>
@@ -15,10 +15,14 @@
         </li>
       </ul>
     </div>
+    <confirm-modal :show="openConfirmModal" :content="confirmContent" @onDelete="onDelete" @close="openConfirmModal = false"></confirm-modal>
   </div>
 </template>
 
 <script>
+import ConfirmModal from '../../utilities/confirmModal/ConfirmModal'
+import ConfirmData from '../../utilities/confirmModal/ConfirmData.json'
+
 export default {
   name: 'Tag',
   props: ['show'],
@@ -26,14 +30,24 @@ export default {
     return {
       msg: 'Tag Page',
       tagData: [],
+      openConfirmModal: false,
+      confirmContent: {},
+      deleteTagId: 0,
+      tagName: '',
     }
   },
   mounted () {
-    this.getTag()
+    this.getTag();
+  },
+  watch: {
+    // openConfirmModal () {
+    //   this.getTag();
+    //   console.log('getTag')
+    // }
   },
   methods: {
     backFunc () {
-      this.$emit('close')
+      this.$emit('close');
     },
     getTag () {
       this.$http.get(`/tags/`, {
@@ -48,23 +62,35 @@ export default {
     addTag () {
       console.log('태그 추가')
       this.$http.post(`/tags/`, {
-        addresses: this.addressArray,
-        dates: this.dateArray,
-        digits: this.phoneArray,
-        emails: this.emailArray,
-        memo: this.memoContents,
-        name: this.name,
-        photo: this.photoArray,
-        type: this.type,
-        urls: this.urlArray
+        name: this.tagName
       }).then((result => {
-          console.log('연락처 생성 성공')
-          this.$emit('close');
+          console.log('태그 생성 성공')
         }))
         .catch(error => {
           alert('에러가 발생했습니다.')
         })
     },
+    alertDeleteTag (name, id) {
+      this.deleteTagId = id;
+      this.openConfirmModal = true;
+      this.confirmContent = ConfirmData[name];
+    },
+    onDelete (isDelete) {
+      if (isDelete) {
+        this.$http.delete(`/tags/${this.deleteTagId}`, {
+        }).then((result => {
+          console.log('태그 삭제')
+
+      this.getTag();
+        }))
+        .catch(error => {
+          alert('에러가 발생했습니다.')
+        })
+      }
+    },
+  },
+  components: {
+    ConfirmModal
   }
 }
 </script>
---
+++ src/utilities/confirmModal/ConfirmModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="confirmModal" v-if="show">
+    <div class="popUpGridWrapepr">
+      <div class="title">
+        <span v-html="content.title"></span>
+      </div>
+
+      <!-- 태그 삭제 -->
+      <div class="buttons" v-if="content.type === 1">
+        <!-- 취소 -->
+        <button class="btn btn-secondary" @click="$emit('close')">{{ content.button1 }}</button>
+        <!-- 삭제하기 -->
+        <button class="btn btn-danger" @click="deleteTag">{{ content.button2 }}</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'confirmModal',
+  props: ['show', 'content'],
+  data () {
+    return {
+      msg: 'confirmModal Page',
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+    deleteTag () {
+      this.$emit('onDelete', true);
+      this.$emit('close');
+    }
+    
+  }
+}
+</script>
+
+<style lang="scss">
+    @import "ConfirmModal.scss"
+</style>
